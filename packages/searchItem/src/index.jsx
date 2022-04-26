315e1a2e1440e743b6cbc125b89f2c492be4e6d7
YDzarks <1024183897@qq.com>
2022-04-26 11:08:15 +0800

    style fix&ELfSearchItem support custom el-col prop

--- packages/searchItem/src/index.jsx
+++ packages/searchItem/src/index.jsx
@@ -1,11 +1,33 @@
 export default {
   name: "ElfSearchItem",
   inject: ["elfSearchForm"],
+  props: {
+    xs: {
+      type: [Number, Object],
+      default: 12,
+    },
+    sm: {
+      type: [Number, Object],
+      default: 12,
+    },
+    md: {
+      type: [Number, Object],
+      default: 8,
+    },
+    lg: {
+      type: [Number, Object],
+      default: 6,
+    },
+    xl: {
+      type: [Number, Object],
+      default: 6,
+    },
+  },
   render() {
     let labelWidth = this.labelWidth ?? this.elfSearchForm.labelWidth;
 
     return (
-      <el-col xl={6} lg={6} md={8} xs={12} sm={12}>
+      <el-col xl={this.xl} lg={this.lg} md={this.md} xs={this.xs} sm={this.sm}>
         <el-form-item props={this.$attrs} labelWidth={labelWidth}>
           {this.$slots.default}
         </el-form-item>
